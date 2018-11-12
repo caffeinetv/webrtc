34ea833ada1775c808a506a41635f0b94f893f82
Matthew Orlando <cogwheel@caffeine.tv>
2018-11-12 13:45:28 -0800

    Use x264 encoding (only win64 currently)

--- caffeine/interface.cc
+++ caffeine/interface.cc
@@ -12,18 +12,49 @@
 
 #include "audiodevice.h"
 #include "stream.h"
+#include "x264encoder.h"
 
-#include "api/peerconnectioninterface.h"
 #include "api/audio_codecs/builtin_audio_decoder_factory.h"
 #include "api/audio_codecs/builtin_audio_encoder_factory.h"
+#include "api/peerconnectioninterface.h"
 #include "api/video_codecs/builtin_video_decoder_factory.h"
 #include "api/video_codecs/builtin_video_encoder_factory.h"
+#include "api/video_codecs/sdp_video_format.h"
 
+#include "media/base/h264_profile_level_id.h"
+#include "media/base/mediaconstants.h"
 #include "modules/audio_processing/include/audio_processing.h"
 
 #include "rtc_base/thread.h"
 
 namespace caff {
+
+// TODO: Use hardware encoding on low powered cpu
+class EncoderFactory : public webrtc::VideoEncoderFactory {
+ public:
+  virtual ~EncoderFactory() {}
+
+  virtual std::vector<webrtc::SdpVideoFormat> GetSupportedFormats()
+      const override {
+    auto profileId = webrtc::H264::ProfileLevelId(
+        webrtc::H264::kProfileConstrainedBaseline, webrtc::H264::kLevel3_1);
+    auto name = cricket::kH264CodecName;
+    auto profile_string = webrtc::H264::ProfileLevelIdToString(profileId);
+    std::map<std::string, std::string> parameters = {
+        {cricket::kH264FmtpProfileLevelId, profile_string.value()}};
+    return {webrtc::SdpVideoFormat(name, parameters)};
+  }
+
+  virtual CodecInfo QueryVideoEncoder(
+      const webrtc::SdpVideoFormat& format) const override {
+    return {false, false};
+  }
+
+  virtual std::unique_ptr<webrtc::VideoEncoder> CreateVideoEncoder(
+	  const webrtc::SdpVideoFormat& format) override {
+    return std::make_unique<X264Encoder>(cricket::VideoCodec(format));
+  }
+};
 
 Interface::Interface() {
   networkThread = rtc::Thread::CreateWithSocketServer();
@@ -45,7 +76,7 @@ Interface::Interface() {
       networkThread.get(), workerThread.get(), signalingThread.get(),
       audioDevice, webrtc::CreateBuiltinAudioEncoderFactory(),
       webrtc::CreateBuiltinAudioDecoderFactory(),
-      webrtc::CreateBuiltinVideoEncoderFactory(),
+      rtc::MakeUnique<EncoderFactory>(),
       webrtc::CreateBuiltinVideoDecoderFactory(), nullptr, nullptr);
 }
 
